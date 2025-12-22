d2c6585e894d1c19e5b43383c34bc598dcefb9ce
Alexander S <aseo35@gatech.edu>
2025-12-22 08:49:24 -0500

    g

--- heatmap.py
+++ heatmap.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 
 df = pd.read_csv("AAPL History")
 
 data = np.random.random((12,12))
-print(data)
+# print(data)
 
-fig, ax = plt.subplots()
+st.title("Interactive Call and Put Model (Heatmap Included)")
+st.line_chart(df["Close"])
+st.chat_input("Variance")
